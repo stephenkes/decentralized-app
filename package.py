28dfc59fe3290a3b589d58da97a07958e2887835
stephenkes <stephenketch453@gmail.com>
2024-07-01 11:24:58 -0700

    stephen

---
+++ package.py
@@ -0,0 +1,11 @@
+# import requests
+#
+# response = requests.get('http://api.github.com')
+# print(response.status_code)
+
+# print(response.json())
+
+import numpy as py
+array = py.array([1, 7, 5, 8, 6])
+array_square = py.square(array)
+print(array_square)
